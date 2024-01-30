9b56e6a3fea8247187ef03f674e53536b12a41a7
Mary Melnikova <marymelnikova1988@gmail.com>
2024-01-30 14:01:55 +0300

    приложение users 22.2 Аутентификация

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'main',
     'materials',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+AUTH_USER_MODEL = 'users.USER'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
